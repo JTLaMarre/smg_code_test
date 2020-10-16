99ae53af5622777aef65b2a5d16d3ab69a971b97
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-10-16 19:24:34 -0400

    starting to set up firebase first time using

--- server.js
+++ server.js
@@ -18,4 +18,4 @@ app.get('*', (req, res) => {
 const port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log(`,enu component listening on ${port}`);
+console.log(` listening on ${port}`);
