c972ca06f10a74f015162f990e5fd9a652dda105
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-10-17 14:43:43 -0400

    more firebase add ons

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,15 @@
 const functions = require('firebase-functions');
 
 // // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.onFileChange = functions.storage.object().onChange(e => {
+exports.onFileChange = functions.storage.object().onFinalize(e => {
 console.log(e)
 return
+})
+
+exports.fileUpload = functions.https.onRequest((req,res)=>{
+res.send(200).json({
+    message:"it worked!"
+})
 })
